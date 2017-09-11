a625d667c47e2198328997fc27657c4200c679da
Cruyun <1045671796@qq.com>
2017-09-11 23:49:44 +0800

    1035

--- PAT乙级/1035.cpp
+++ PAT乙级/1035.cpp
@@ -1,107 +1,56 @@
 #include <iostream>
-#include <string.h>
+#include <algorithm>
 using namespace std;
 
-void merge(int * a, int lo, int hi) {
-    int mid, i, j, k;
-    int aux[100];
-
-    for (i = 0; i <= hi; i++) {
-        aux[i] = a[i];
-    }
-
-    mid = (lo + hi) / 2;
-    i = lo;
-    j = mid + 1;
-
-    for (k = lo; k <= hi; k++) {
-        if (i > mid) {
-            a[k] = aux[j++];
-        } else if (j > hi) {
-            a[k] = aux[i++];
-        } else if (aux[j] < aux[i]) {
-            a[k] = aux[j++];
-        } else {
-            a[k] = aux[i++];
-        }
-    }
-}
-
-bool comp(int * a, int * b, int n) {
-    for (int i = 0; i < n; i++) {
-        if (a[i] != b[i])
-            return true;
-    }
-    return 0;
-}
-
-void printArray(int * a, int n) {
-    for (int i = 0; i < n; i++) {
-        cout << a[i];
-        if (i != n-1)
-            cout<< " ";
-        else
-            cout << endl;
-    }
-}
-int min(int x, int y) {
-    if (x <= y)
-        return x;
-    else
-        return y;
+bool cmp(int a, int b) {
+    return a < b;
 }
 
 int main() {
-    int a[100], b[100], c[100];
-    int n, i, j,temp;
-    int flag1 = 0;
-    int flag2 = 0;
-
+    int n;
     cin >> n;
+
+    int * a = new int [n];
+    int * b = new int [n];
+
+    int i, j;
+
     for (i = 0; i < n; i++) {
         cin >> a[i];
     }
     for (i = 0; i < n; i++) {
         cin >> b[i];
     }
-    memcpy(c, a, n*4);
 
-    // insertion sort
-    for (i = 1; i < n; i++) {
-        j = i;
-        temp = c[j];
-        while (temp < c[j-1] && j > 0) {
-            c[j] = c[j-1];
-            j--;
-        }
-        c[j] = temp;
-
-        if (flag1 == 1) {
-            if (comp(c, b, n) == 0)
-                continue;
-            cout<< "Insertion Sort" <<endl;
-            printArray(c, n);
-            return 0;
-        }
-        if (comp(c, b, n) == 0) {
-            flag1 = 1;
+    for (i = 0; i < n -1 && b[i] <= b[i + 1]; i++);
+    for (j = i + 1; a[j] == b[j] && j < n; j++);
+    if (j == n) {
+        cout << "Insertion Sort" << endl;
+        sort(a, a+i+2, cmp);
+    } else {
+        cout << "Merge Sort" << endl;
+        int k = 1;
+        int flag = 1;
+        while (flag) {
+            flag = 0;
+            for (i = 0; i < n; i++) {
+                if (a[i] != b[i]) {
+                    flag = 1;
+                }
+            }
+            k = k * 2;
+            for (i = 0; i < n / k; i++) {
+                sort(a + i * k, a + (i + 1) * k, cmp);
+            }
+            sort(a + n / k * k, a + n, cmp);
         }
     }
-
-    memcpy(c, a, n*4);
-
-    // merge sort
-    for (i = 1; i < n; i *= 2) {
-        for (j = 0; j < n; j += 2 * i) {
-            merge(c, j, min(j+2*i-1, n-1));
-        }
-        if (flag2 == 1) {
-            cout << "Merge Sort" << endl;
-            printArray(c, n);
-            return 0;
-        }
-        if (comp(c, b, n) == 0)
-            flag2 = 1;
+    for (j = 0; j < n - 1; j++) {
+        cout << a[j] << " ";
     }
+    cout << a[n - 1];
+    delete [] a;
+    delete [] b;
     return 0;
+
 }
