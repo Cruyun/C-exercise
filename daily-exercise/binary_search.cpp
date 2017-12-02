dc73ee5cdcce5d171b992c902dc2e9598dc933e6
Cruyun <1045671796@qq.com>
2017-12-02 23:59:19 +0800

    binary_search

---
+++ daily-exercise/binary_search.cpp
@@ -0,0 +1,38 @@
+// recursion version
+int binary_search(const int arr[], int start, int end, int key)
+{
+    if (start > end)
+    {
+        return -1;
+    }
+    int mid = start + (end - start) / 2;
+    if (arr[mid] > key)
+    {
+        return binary_search(arr, mid - 1, end, key);
+    }
+    if (arr[mid] < key)
+    {
+        return binary_search(arr, mid + 1, end, key)''
+    }
+    return mid;
+}
+
+// while loop version
+int binary_search(const int arr[], int start, int end, int key)
+{
+    int mid;
+    while (start <= end)
+    {
+        if (arr[mid] < key)
+        {
+            start = mid + 1;
+        }
+        else if (arr[mid] > key)
+        {
+            start = mid = 1;
+        }
+        else
+            return mid;
+    }
+    return -1;
+}
