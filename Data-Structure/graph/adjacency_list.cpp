bac8fdfc4c58c8f8d5e01b9d1740eac247a6653b
Cruyun <1045671796@qq.com>
2017-12-05 23:50:36 +0800

    graph

---
+++ Data-Structure/graph/adjacency_list.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#define MAX_VERTEX_NUM  20
+#define INFINITY 65535
+
+typedef double InfoType;
+typedef char VertexType;
+
+typedef enum {DG, DN, AG} GraphKind;
+
+typedef struct AcNode {
+    int      adjvex;
+    struct   AcNode *nextarc;
+    InfoType info;
+} ArcNode;
+
+typedef struct VNode {
+    VertexType data;
+    ArcNode *firstarc;
+    InfoType info;
+} VNode, AdjList[MAX_VERTEX_NUM];
+
+typedef struct {
+    AdjList vertices;
+    int     vexnum, arcnum;
+    int     kind;
+}ALGraph;
+
+typedef struct {
+    VertexType V1;
+    VertexType V2;
+    InfoType adj;
+}VTable;
+
+const int vexnum = 9;
+const int arcnum = 8;
+VTable vTable[arcnum] = { {'A','B',32.8},{'A','H',12.1},{'B','C',5.9},{'C','D',21.3},
+                          {'C','E',41.1}, {'E','G',10.5}, {'F','I',79.2},{'H','I',8.7} };
+
+VertexType vexTable[vexnum + 1] = {'A','B','C','D','E','F','G','H','I'};
+
+void CreateUDG(ALGraph &G, VTable *vTable, VertexType *vexTable) {
+    VertexType v1, v2;
+
+    G.vexnum = vexnum;
+    G.arcnum = arcnum;
+
+    for (int i = 0; i < G.arcnum; ++i) {
+        G.vertices[i].data = vexTable[i];
+        G.vertices[i].firstarc = NULL;
+    }
+
+    for (int k = 0; k < G.arcnum; ++k) {
+        v1 = vTable[k].V1;
+        cout << "v1: " << v1 << endl;
+        v2 = vTable[k].V2;
+        cout << "v2: " << v2 << endl;
+        int i = vTable[k].V1 - 'A';
+        int j = vTable[k].V2 - 'A';
+
+        ArcNode *pi = new ArcNode;
+        pi->adjvex = j;
+        pi->nextarc = G.vertices[i].firstarc;
+        G.vertices[i].firstarc = pi;
+
+        ArcNode *pj = new ArcNode;
+        pj->adjvex = i;
+        pj->nextarc = G.vertices[j].firstarc;
+        G.vertices[j].firstarc = pj;
+    }
+}
+
+bool visited[vexnum];
+
+void DFS(ALGraph &G, int i){
+    visited[i] = true;
+    cout << i << ": " << G.vertices[i].data << " ";
+
+    ArcNode *p = G.vertices[i].firstarc;
+
+    while(p){
+        if(!visited[p->adjvex]){
+            DFS(G,p->adjvex);
+        }
+        p= p->nextarc;
+    }
+}
+
+
+void DFSTraverse(ALGraph &G){
+    for (int i = 0; i < G.vexnum; ++i){
+        visited[i] = false;
+    }
+    for (int i = 0; i < G.vexnum; ++i){
+        if(!visited[i]){
+            DFS(G,i);
+        }
+    }
+}
+
+int main() {
+    ALGraph *G = new ALGraph;
+    CreateUDG(*G, vTable, vexTable);
+    DFSTraverse(*G);
+    return 0;
+}
---
+++ Data-Structure/graph/adjacency_matrix.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#define MAX_VERTEX_NUM  20
+#define INFINITY 65535
+
+typedef double InfoType;
+typedef char VertexType;
+
+typedef enum {DG, DN, AG} GraphKind;
+
+typedef struct {
+    VertexType vex[MAX_VERTEX_NUM]; //描述顶点的数组
+    InfoType adjMatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM]; //邻接矩阵
+    int       vexnum, arcnum; //图的当前顶点数和弧数
+    GraphKind kind;
+} MGraph;
+
+typedef struct {
+    VertexType V1;
+    VertexType V2;
+    InfoType adj;
+}VTable;
+
+const int vexnum = 9;
+const int arcnum = 15;
+VTable vTable[arcnum] = { {'A','B',32.8},{'A','C',44.6},{'A','H',12.1},{'A','I',18.2},
+                          {'B','C',5.9} ,{'C','G',56.4},{'C','E',41.1},{'C','D',21.3},
+                          {'D','E',67.3},{'D','F',98.7},{'E','F',85.6},{'E','G',10.5},
+                          {'F','I',79.2},{'G','H',52.5},{'H','I',8.7} };
+
+VertexType vexTable[vexnum + 1] = {'A','B','C','D','E','F','G','H','I'};
+
+void CreateGraph(MGraph *g, VTable *vTable, VertexType *vexTable) {
+    int i, j, k;
+
+    g->arcnum = arcnum;
+    g->vexnum = vexnum;
+
+    for (i = 0; i < g->vexnum; ++i) {
+        g->vex[i] = vexTable[i];
+    }
+
+    // 矩阵初始化
+    for (i = 0; i < g->vexnum; ++i) {
+        for (j = 0; j < g->vexnum; ++j) {
+            if (i == j)
+                g->adjMatrix[i][j] = 0;
+            else
+                g->adjMatrix[i][j] = INFINITY;
+        }
+    }
+
+    for (k = 0; k < g->arcnum; ++k) {
+        VertexType v1, v2;
+        InfoType weight;
+        v1 = vTable[k].V1;
+        v2 = vTable[k].V2;
+        weight = vTable[k].adj;
+
+        g->adjMatrix[v1 - 'A'][v2 - 'A'] = weight;
+        g->adjMatrix[v2 - 'A'][v1 - 'A'] = weight;
+    }
+}
+
+void ShowMatrix(MGraph *g) {
+    int i, j;
+    for (i = 0; i < g->vexnum; i++) {
+        for (j = 0; j < g->vexnum; j++) {
+            if (g->adjMatrix[i][j] == INFINITY) {
+                g->adjMatrix[i][j] = 0;
+            }
+            cout << setw(4) << g->adjMatrix[i][j] << "  ";
+            if (j == g->vexnum - 1)
+                cout << endl;
+        }
+    }
+}
+bool visited[vexnum];
+void DFS(MGraph &G, int i) {
+    int j;
+    visited[i] = true;
+    cout << G.vex[i] << " ";
+
+    for (j = 0; j < G.vexnum; ++j) {
+        if (!G.adjMatrix[i][j]  &&  !visited[j])
+            DFS(G, j);
+    }
+}
+
+void DFSTraverse(MGraph &G) {
+    for (int i = 0; i < G.vexnum; ++i)
+        visited[i] = false;
+
+    for (int i = 0; i < G.vexnum; ++i) {
+        if (!visited[i])
+            DFS(G, i);
+    }
+
+}
+
+int main() {
+    MGraph *g = new MGraph;
+    CreateGraph(g, vTable, vexTable);
+    cout << "the Adjacency matrix of this graph:" << endl;
+    ShowMatrix(g);
+    cout << endl;
+    cout << "show this graph:" << endl;
+    DFSTraverse(*g);
+    return 0;
+}
