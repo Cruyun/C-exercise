fdb371414b5c70dbc78d09da56442d89df840d57
Cruyun <1045671796@qq.com>
2018-01-02 13:26:45 +0800

    fix error

--- Data-Structure/sort_aglorihm/main.cpp
+++ Data-Structure/sort_aglorihm/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+//#include "insertion.cpp"
 #include "selection.cpp"
 using namespace std;
 
@@ -6,5 +7,9 @@ int main() {
 	int arr[7] = {9, 7, 3, 4, 2, 8, 1};
 	int length = 7;
 	selection_sort(arr,length);
+	for (int i = 0; i < length; i++) {
+		cout << "arr[" << i << "]: " << arr[i] << endl;
+	}
+
 	return 0;
 }
--- Data-Structure/sort_aglorihm/selection.cpp
+++ Data-Structure/sort_aglorihm/selection.cpp
@@ -3,21 +3,18 @@ using namespace std;
 
 void selection_sort(int arr[], int length) {
 	int temp;
-	for (int i = 1; i < length; i++) {
+	for (int i = 0; i < length; i++) {
 		int j = i;
-		for ( int k = i + 1; k <= length; k++) {
+		for ( int k = i + 1; k < length; k++) {
 			if (arr[k] < arr[j]) {
 				j = k;
 			}
-			if ( j != i) {
-				temp = arr[j];
-				arr[j] = arr[k];
-				arr[k] = temp;
-			}
+		}
+		if ( j != i) {
+			temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
 		}
 	}
-	for (int i = 0; i < length; i++) {
-		cout << i << ":" << arr[i] << endl;
-	}	
 }
 
