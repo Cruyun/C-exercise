dee73b96b87e3cabdb4730e4e17617007eb8a459
Cruyun <1045671796@qq.com>
2018-03-28 00:14:41 +0800

    1082

---
+++ 1082.cpp
@@ -0,0 +1,37 @@
+// 部分正确。。应该有可能并列的
+
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct Players {
+    string id;
+    int x;
+    int y;
+    int distance;
+};
+
+int cmp(struct Players a, struct Players b) {
+    return a.distance < b.distance ? a.distance : b.distance;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<Players> player(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> player[i].id >> player[i].x >> player[i].y;
+
+        player[i].distance = abs(player[i].x) * abs(player[i].x) + abs(player[i].y) * abs(player[i].y);
+    }
+
+    sort(player.begin(), player.end(), cmp);
+    cout << player[0].id << " " << player[n-1].id;
+
+    return 0;
+}
+
